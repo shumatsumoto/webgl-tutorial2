a8baf39773815c18af50f006f5428e99b508fb6f
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-23 11:46:40 +0900

    getBoundingClientRectを使ってみよう

--- 01_integration/src/010_positioning/start/index.js
+++ 01_integration/src/010_positioning/start/index.js
@@ -1,9 +1,47 @@
-import { WebGLRenderer } from "three";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Scene,
+  WebGLRenderer,
+} from "three";
 import "./style.scss";
+
+const world = {};
 
 init();
 function init() {
   const canvas = document.querySelector("#canvas");
-  const renderer = new WebGLRenderer();
-  console.log(canvas);
+  const canvasRect = canvas.getBoundingClientRect();
+  console.log(canvasRect);
+  world.renderer = new WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  world.renderer.setSize(canvasRect.width, canvasRect.height, false);
+  world.renderer.setPixelRatio(window.devicePixelRatio);
+  world.renderer.setClearColor(0x000000, 0);
+
+  world.scene = new Scene();
+  world.camera = new PerspectiveCamera(
+    75,
+    canvasRect.width / canvasRect.height,
+    0.1,
+    1000
+  );
+  world.camera.position.z = 5;
+
+  const geometry = new PlaneGeometry(1, 1);
+  const material = new MeshBasicMaterial({ color: 0xff0000 });
+  const mesh = new Mesh(geometry, material);
+
+  world.scene.add(mesh);
+
+  animate();
+  function animate() {
+    requestAnimationFrame(animate);
+    world.renderer.render(world.scene, world.camera);
+  }
 }
