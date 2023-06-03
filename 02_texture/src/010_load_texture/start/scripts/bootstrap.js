3b19aabe7b8b79e627c64f2c439e18b32c3d265a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-06-03 12:13:08 +0900

    テクスチャをキャッシュとして保持しよう

--- 02_texture/src/010_load_texture/start/scripts/bootstrap.js
+++ 02_texture/src/010_load_texture/start/scripts/bootstrap.js
@@ -2,6 +2,7 @@ import world from "./glsl/world";
 import { viewport } from "./helper";
 import scroller from "./component/scroller";
 import mouse from "./component/mouse";
+import loader from "./component/loader";
 
 export function init() {
   const canvas = document.querySelector("#canvas");
@@ -9,13 +10,11 @@ export function init() {
 
   scroller.init();
 
+  loader.loadAllAssets();
+
   world.init(canvas, viewport);
 
   mouse.init();
 
   world.render();
 }
-
-
-
-
